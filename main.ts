23e8255c16fdb9fa40554a8e743e169cab03bd79
SebastiaanWouters <sebastiaanwouters@tutanota.com>
2024-11-07 23:54:25 +0100

    remove api key

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { download, search } from "./mod.ts";
+import "jsr:@std/dotenv/load";
 
 const books = await search("antibodies");
 for (const book of books) {
@@ -17,12 +18,13 @@ for (const paper of papers) {
 
 console.log("\n----------------------------------------------------------- \n");
 
-const dl = await download(books[0].md5, "6WgoiFL2scCsV3wmmcEEmTd7uMzAC");
+const dl = await download(books[0].md5, Deno.env.get("API_KEY"));
 if (dl.error) {
   console.error(dl.error.message);
+} else {
+  console.log(JSON.stringify(dl));
+  await Deno.writeFile(
+    "./download." + dl.result?.extension,
+    new Uint8Array(dl.result!.content),
+  );
 }
-console.log(JSON.stringify(dl));
-await Deno.writeFile(
-  "./download." + dl.result?.extension,
-  new Uint8Array(dl.result!.content),
-);
